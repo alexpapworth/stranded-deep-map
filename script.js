6421a7b795dcaadd0482cfc9675dfe4573671328
Alex Papworth <arpapworth@gmail.com>
2020-05-03 14:34:20 +0100

    Add interactable tile options

--- script.js
+++ script.js
@@ -1,9 +1,56 @@
-"use strict";
+"use strict"
 
-let ready;
+function selectOptionToAddToTile() {
+  if (document.querySelector('.option.selected')) {
+    unselectOption.call(document.querySelector('.option.selected'))
+  }
 
-ready = function() {
-  
+  this.removeEventListener('click', selectOptionToAddToTile)
+  this.addEventListener('click', unselectOption)
+
+  this.classList.add('selected')
+
+  prepareTilesForClicking()
 }
 
-document.addEventListener('DOMContentLoaded', ready);
+function unselectOption() {
+  this.removeEventListener('click', unselectOption)
+  this.addEventListener('click', selectOptionToAddToTile)
+
+  let option = this.innerText
+  this.classList.remove('selected')
+
+  unprepareTilesForClicking()
+}
+
+function prepareTilesForClicking() {
+  for (var i = 0; i < document.querySelectorAll('.cell').length; i++) {
+    document.querySelectorAll('.cell')[i].classList.add('clickable')
+    document.querySelectorAll('.cell')[i].addEventListener('click', appendOptionToTile)
+  }
+}
+
+function unprepareTilesForClicking() {
+  for (var i = 0; i < document.querySelectorAll('.cell').length; i++) {
+    document.querySelectorAll('.cell')[i].classList.remove('clickable')
+  }
+}
+
+function appendOptionToTile() {
+  let option = document.querySelector('.option.selected').innerText
+  this.innerText = this.innerText + "\n" + option
+}
+
+let ready
+
+ready = function() {
+  for (var i = 0; i < document.querySelectorAll('.option').length; i++) {
+    document.querySelectorAll('.option')[i].addEventListener('click', selectOptionToAddToTile)
+  }
+
+  for (var i = 0; i < document.querySelectorAll('.cell').length; i++) {
+    document.querySelectorAll('.cell')[i]
+  }
+}
+
+document.addEventListener('DOMContentLoaded', ready)
